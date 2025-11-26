6b916d7b11a0f8649096d024db6ff3fc4868fc9a
SageUA <panchyshyn33@gmail.com>
2025-11-26 13:11:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const li = [...document.querySelectorAll('ul li')];
+
+const convertToNumber = (string) => {
+  const validNumber = +string.slice(1).replaceAll(',', '');
+
+  if (!isNaN(validNumber)) {
+    return validNumber;
+  }
+
+  return NaN;
+};
+
+const sortList = (list) => {
+  const ul = list[0].parentElement;
+
+  const employees = getEmployees(list);
+
+  employees.sort((a, b) => {
+    return convertToNumber(b.salary) - convertToNumber(a.salary);
+  });
+
+  ul.innerHTML = '';
+
+  employees.forEach(({ el }) => ul.appendChild(el));
+};
+
+const getEmployees = (list) => {
+  const result = list.map((el) => {
+    return {
+      el,
+      name: el.textContent.trim(),
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+
+  return result;
+};
+
+sortList(li);
